04acaa779ff535edcac1ff0680165b5a4a000872
Scott <scott@boatrockr.online>
2025-03-10 15:03:55 +1100

    event service created and wired in

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,37 +1,19 @@
 <script setup>
 import EventCard from '../components/EventCard.vue';
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
+import EventService from '@/services/eventService';
 
-const events = ref([{
-  id: 5928101,
-  category: "animal welfare",
-  organizer: "Adam",
-  title: "Cat Cabaret",
-  description: "Yay felines!",
-  location: "Meow Town",
-  date: "14 February, 2025",
-  time: "2:00",
-},
-{
-  id: 8419988,
-  category: "animal welfare",
-  organizer: "Adam",
-  title: "Kitty Cluster",
-  description: "Yay cats!",
-  location: "Catlandia",
-  date: "January 28, 2025",
-  time: "7:00",
-},
-{
-  id: 4582797,
-  category: "animal welfare",
-  organizer: "Adam",
-  title: "Puppy Parade",
-  description: "Yay pups!",
-  location: "Puptown ",
-  date: "March 14, 2025",
-  time: "1:00",
-}])
+const events = ref(null)
+
+onMounted(() => {
+  EventService.getEvents()
+    .then((response) => {
+      events.value = response.data
+    }).catch((error) => {
+      console.log(error)
+    })
+})
+
 </script>
 
 <template>
