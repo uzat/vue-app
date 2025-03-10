bc1d1aa964fa823f3ff3603a7ecea0dddc18e06d
Scott <scott@boatrockr.online>
2025-03-10 20:12:08 +1100

    typo in event service

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: 'http://my-json-server.typicode.com/uzat/vuedb',
+  baseURL: 'https://my-json-server.typicode.com/uzat/vuedb',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
