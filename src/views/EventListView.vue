5644ffdadd1af603ba637db2951f6e0fa327405f
Scott <scott@boatrockr.online>
2025-03-11 17:38:09 +1100

    restyle next and previous

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -44,11 +44,14 @@ watch(
   <h1>Events for Good</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
-    <router-link :to="{ name: 'event-list', query: { page: page - 1 } }" rel="prev" v-if="page != 1">Prev
-      Page</router-link>
+    <div class="pagination">
+      <router-link id="page-prev" :to="{ name: 'event-list', query: { page: page - 1 } }" rel="prev"
+        v-if="page != 1">&#60; Previous</router-link>
 
-    <router-link :to="{ name: 'event-list', query: { page: page + 1 } }" rel="next" v-if="hasNextPage">Next
-      Page</router-link>
+      <router-link id="page-next" :to="{ name: 'event-list', query: { page: page + 1 } }" rel="next" v-if="hasNextPage">
+        Next &#62;</router-link>
+
+    </div>
   </div>
 </template>
 
@@ -57,5 +60,24 @@ watch(
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.pagination {
+  display: flex;
+  width: 290px;
+}
+
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-prev {
+  text-align: left;
+}
+
+#page-next {
+  text-align: right;
 }
 </style>
