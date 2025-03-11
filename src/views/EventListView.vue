e005222d046f55d46e3b1b7da2e144ffc2d2aa08
Scott <scott@boatrockr.online>
2025-03-11 17:08:23 +1100

    pagination and onmount watch fix

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,18 +1,32 @@
 <script setup>
 import EventCard from '../components/EventCard.vue';
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 import EventService from '@/services/EventService';
+
+const props = defineProps(["page"])
 
 const events = ref(null)
 
-onMounted(() => {
-  EventService.getEvents()
+const fetchEvents = () => {
+  EventService.getEvents(2, props.page)
     .then((response) => {
       events.value = response.data
     }).catch((error) => {
       console.log(error)
     })
+}
+
+onMounted(() => {
+  fetchEvents()
 })
+
+// to fix the events not loading after the page is mounted
+watch(
+  () => props.page, () => {
+    events.value = null
+    fetchEvents()
+  }
+)
 
 </script>
 
@@ -20,6 +34,10 @@ onMounted(() => {
   <h1>Events for Good</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <router-link :to="{ name: 'event-list', query: { page: page - 1 } }" rel="prev" v-if="page != 1">Prev Page
+    </router-link>
+
+    <router-link :to="{ name: 'event-list', query: { page: page + 1 } }" rel="next">Next Page</router-link>
   </div>
 </template>
 
